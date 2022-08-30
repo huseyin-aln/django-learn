c14decad059694e258fe26d5261c79d1bf0941e6
huseyin-aln <huseyinarslan06@gmail.com>
2022-08-30 20:37:15 +0300

    class_based_views

---
+++ 11-ToDo-App-CBV/todo/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import TodoList, home, todo_create, todo_update, todo_delete
+from .models import Todo
+from django.views.generic import ListView
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("add/", todo_create, name="add"),
+    path('list/',TodoList.as_view(),name="list"),
+    path('lists/', ListView.as_view(model = Todo,context_object_name = 'todos')),
+    path("update/<int:id>", todo_update, name="update"),
+    path("delete/<int:id>", todo_delete, name="delete"),
+]
